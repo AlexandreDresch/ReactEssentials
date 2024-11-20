001d0c403f2c0e9e2d54056f7ff9093c34b78a70
AlexandreDresch <connectWithAlexandre@proton.me>
2024-11-20 00:42:53 -0300

    feat: add ImageCarousel component

---
+++ src/components/image-carousel.tsx
@@ -0,0 +1,52 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
+
+export default function ImageCarousel() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  return (
+    <div className="mx-auto max-w-5xl">
+      <Carousel setApi={setApi} className="w-full max-w-5xl">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Card className="rounded-none bg-slate-600 border-none">
+                <CardContent className="flex aspect-video items-center justify-center p-6 rounded-none">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="bg-transparent text-white border-none size-8 hover:bg-transparent hover:text-white"/>
+        <CarouselNext className="bg-transparent text-white border-none size-8 hover:bg-transparent hover:text-white"/>
+      </Carousel>
+      <div className="py-2 text-center text-sm text-muted-foreground">
+        Slide {current} of {count}
+      </div>
+    </div>
+  );
+}
