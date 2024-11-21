ea19d1a6964b178d0cd784afc481613ef66ab1b1
AlexandreDresch <connectWithAlexandre@proton.me>
2024-11-21 00:33:07 -0300

    feat: add Heading component

---
+++ src/components/heading.tsx
@@ -0,0 +1,32 @@
+import { ChevronRightIcon } from "lucide-react";
+import { Separator } from "./ui/separator";
+
+export default function Heading({
+  title,
+  link,
+}: {
+  title: string;
+  link?: string;
+}) {
+  return (
+    <div className="flex w-full items-center justify-center xl:justify-between relative h-60">
+      <Separator className="w-1/4 h-32 bg-white hidden xl:block" />
+
+      <h3 className="z-10 px-6 font-bold text-white text-7xl sm:text-8xl md:text-9xl lg:text-[200px] text-center uppercase">
+        {title}
+      </h3>
+      <div className="xl:w-1/4 relative h-full">
+        <Separator className="h-36 bg-black border-[1px] border-white absolute top-0 right-10 hidden xl:block" />
+        <Separator className="h-32 bg-white absolute top-16 hidden xl:block" />
+        {link && (
+          <a
+            href={link}
+            className="text-white font-josefin font-bold xl:text-black absolute bottom-10 xl:bottom-24 xl:right-36 flex gap-2 uppercase"
+          >
+            <span>More</span> <ChevronRightIcon />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
